4ef192820c7c1dd657d32af9af8ccc5068212bc6
t_liuyu <yufei.liu@autodesk.com>
2016-12-19 18:36:24 +0800

    模块

--- awesome-python3-webapp/iOS/app.py
+++ awesome-python3-webapp/iOS/app.py
@@ -18,3 +18,20 @@ def init(loop):
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global _pool
+    _pool = yield from aiomysql.create_pool{
+        host = kw.get('host','localhost'),
+        port = kw.get('port',3306),
+        user = kw['user'],
+        password = kw['password'],
+        db = kw['db'],
+        charset = kw.get('charset','utf8'),
+        autocommit = kw.get('autocommit',Ture),
+        maxsize = kw.get('maxsize',10),
+        minsize = kw.get('minsize',1),
+        loop = loop
+}
